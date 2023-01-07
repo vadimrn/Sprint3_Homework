7225f3585759418932825af1e7826330f4b16d9f
vadimrn <120738327+vadimrn@users.noreply.github.com>
2023-01-07 20:39:07 +0500

    First commit

--- Приложение счетчик/ViewController.swift
+++ Приложение счетчик/ViewController.swift
@@ -5,15 +5,27 @@
 //  Created by Lala on 05.01.2023.
 //
 
-import UIKit
+import SwiftUI
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+struct SwiftUICounter: View {
+    @State var counterLabel = 0
+    var body: some View {
+        VStack {
+            Text("\(counterLabel)")
+            HStack {
+                Button("Удалить") {
+                    counterLabel = 0
+                }
+                Button("Добавить") {
+                    counterLabel += 1
+                }
+            }
+        }
     }
-
-
 }
 
+struct SwiftUICounter_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUICounter()
+    }
+}
